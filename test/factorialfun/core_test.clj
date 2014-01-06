afa0c0bae766ed1d5c9ffb4658a38cea918b6ba4
Matt Oquist <moquist@majen.net>
2014-01-05 21:47:48 -0500

    Beef up our unit testing to handle the new f3-seq.

--- test/factorialfun/core_test.clj
+++ test/factorialfun/core_test.clj
@@ -3,10 +3,16 @@
             [factorialfun.core :refer :all]))
 
 (deftest sanity?
-  (testing "f0: 4"
-    (is (= 24 (f0 4))))
-  (testing "f1: 4"
-    (is (= 24 (f1 4))))
-  (testing "f2: 4"
-    (is (= 24 (f2 4)))))
+  (testing "fns"
+    (testing "f0"
+      (is (= 24 (f0 4))))
+    (testing "f1"
+      (is (= 24 (f1 4))))
+    (testing "f2"
+      (is (= 24 (f2 4)))))
+  (testing "seqs"
+    (testing "f3-seq-nth"
+      (is (= 24 (f3-seq-n 4))))
+    (testing "f3-seq-take"
+      (is (= '(1 2 6 24 120 720 5040) (f3-seq-take 7))))))
 
